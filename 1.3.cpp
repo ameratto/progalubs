e859577aac703b25c9bf92c49e210c809a946489
ameratto <64779721+ameratto@users.noreply.github.com>
2020-06-11 14:52:55 +0300

    1.3

---
+++ 1.3.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a;
+    cout << "Vvedit' temperaturu za Farengeitom: ";
+    cin >> a;
+    cout << (a - 32) * 5 / 9;
+
+}
+
---
+++ 1.4.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    int a;
+    cout << "Vvedit' simvol: ";
+    cin >> c;
+    a = c;
+    cout << a;
+}
