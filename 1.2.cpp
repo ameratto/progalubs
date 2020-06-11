764d6c5da0bb7ada66a20689893decb18d202894
ameratto <64779721+ameratto@users.noreply.github.com>
2020-06-11 14:51:33 +0300

    1.2

---
+++ 1.2.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Vvedit' pershe chislo:";
+    cin >> a;
+    cout << "Vvedit' druge chislo:";
+    cin >> b;
+    cout << a + b;
+}
